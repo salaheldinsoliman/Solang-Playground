71e7776aedfdc38a3481349d9f4c17014cefd747
salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
2024-01-28 04:20:24 +0200

    add colors
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>

--- packages/app/src/language.ts
+++ packages/app/src/language.ts
@@ -3,9 +3,39 @@ import { MonacoToProtocolConverter, ProtocolToMonacoConverter } from "monaco-lan
 import * as monaco from "monaco-editor-core";
 import * as proto from "vscode-languageserver-protocol";
 import contributes from "./contributes.json";
-import config from "./language_configuration/solidity.configuration.json";
-
 import Client from "./client";
+import { solidityTokensProvider, solidityLanguageConfig } from './language_configuration/solidity_syntax'
+
+const themeType = 'vs-dark';
+const themeName = 'remix-dark';
+const formatColor = (name: string): string => {
+  let color = window.getComputedStyle(document.documentElement).getPropertyValue(name).trim()
+  if (color.length === 4) {
+    color = color.concat(color.substr(1))
+  }
+  console.log('color ', color)
+  return color
+}
+// see https://microsoft.github.io/monaco-editor/playground.html#customizing-the-appearence-exposed-colors
+const lightColor = formatColor('--light')
+const infoColor = formatColor('--info')
+const darkColor = formatColor('--dark')
+const secondaryColor = formatColor('--secondary')
+const primaryColor = formatColor('--primary')
+const textColor = formatColor('--text') || darkColor
+const textbackground = formatColor('--text-background') || lightColor
+
+const blueColor = formatColor('--blue')
+const successColor = formatColor('--success')
+const warningColor = formatColor('--warning')
+const yellowColor = formatColor('--yellow')
+const pinkColor = formatColor('--pink')
+const locationColor = '#9e7e08'
+// const purpleColor = formatColor('--purple')
+const dangerColor = formatColor('--danger')
+const greenColor = formatColor('--green')
+const orangeColor = formatColor('--orange')
+const grayColor = formatColor('--gray')
 
 export const monacoToProtocol = new MonacoToProtocolConverter(monaco);
 export const protocolToMonaco = new ProtocolToMonacoConverter(monaco);
@@ -43,7 +73,130 @@ export default class Language implements monaco.languages.ILanguageExtensionPoin
 
   private registerLanguage(client: Client): void {
     void client;
-    monaco.languages.register(Language.extensionPoint());
+    monaco.languages.register({ id: "solidity" });
+
+    monaco.languages.setMonarchTokensProvider('solidity', solidityTokensProvider as any)
+    monaco.languages.setLanguageConfiguration('solidity', solidityLanguageConfig as any)
+    monaco.editor.defineTheme(themeName, {
+      base: themeType,
+      inherit: true, // can also be false to completely replace the builtin rules
+
+
+
+
+      rules: [
+
+
+        // global variables
+        { token: 'keyword.abi', foreground: blueColor },
+        { token: 'keyword.block', foreground: blueColor },
+        { token: 'keyword.bytes', foreground: blueColor },
+        { token: 'keyword.msg', foreground: blueColor },
+        { token: 'keyword.tx', foreground: blueColor },
+
+        // global functions
+        { token: 'keyword.assert', foreground: blueColor },
+        { token: 'keyword.require', foreground: blueColor },
+        { token: 'keyword.revert', foreground: blueColor },
+        { token: 'keyword.blockhash', foreground: blueColor },
+        { token: 'keyword.keccak256', foreground: blueColor },
+        { token: 'keyword.sha256', foreground: blueColor },
+        { token: 'keyword.ripemd160', foreground: blueColor },
+        { token: 'keyword.ecrecover', foreground: blueColor },
+        { token: 'keyword.addmod', foreground: blueColor },
+        { token: 'keyword.mulmod', foreground: blueColor },
+        { token: 'keyword.selfdestruct', foreground: blueColor },
+        { token: 'keyword.type ', foreground: blueColor },
+        { token: 'keyword.gasleft', foreground: blueColor },
+
+        // specials
+        { token: 'keyword.super', foreground: infoColor },
+        { token: 'keyword.this', foreground: infoColor },
+        { token: 'keyword.virtual', foreground: infoColor },
+
+        // for state variables
+        { token: 'keyword.constants', foreground: grayColor },
+        { token: 'keyword.override', foreground: grayColor },
+        { token: 'keyword.immutable', foreground: grayColor },
+
+        // data location
+        { token: 'keyword.memory', foreground: locationColor },
+        { token: 'keyword.storage', foreground: locationColor },
+        { token: 'keyword.calldata', foreground: locationColor },
+
+        // for Events
+        { token: 'keyword.indexed', foreground: yellowColor },
+        { token: 'keyword.anonymous', foreground: yellowColor },
+
+        // for functions
+        { token: 'keyword.external', foreground: successColor },
+        { token: 'keyword.internal', foreground: successColor },
+        { token: 'keyword.private', foreground: successColor },
+        { token: 'keyword.public', foreground: successColor },
+        { token: 'keyword.view', foreground: successColor },
+        { token: 'keyword.pure', foreground: successColor },
+        { token: 'keyword.payable', foreground: successColor },
+        { token: 'keyword.nonpayable', foreground: successColor },
+
+        // Errors
+        { token: 'keyword.Error', foreground: dangerColor },
+        { token: 'keyword.Panic', foreground: dangerColor },
+
+        // special functions
+        { token: 'keyword.fallback', foreground: pinkColor },
+        { token: 'keyword.receive', foreground: pinkColor },
+        { token: 'keyword.constructor', foreground: pinkColor },
+
+        // identifiers
+        { token: 'keyword.identifier', foreground: warningColor },
+        { token: 'keyword.for', foreground: warningColor },
+        { token: 'keyword.break', foreground: warningColor },
+        { token: 'keyword.continue', foreground: warningColor },
+        { token: 'keyword.while', foreground: warningColor },
+        { token: 'keyword.do', foreground: warningColor },
+        { token: 'keyword.delete', foreground: warningColor },
+
+        { token: 'keyword.if', foreground: yellowColor },
+        { token: 'keyword.else', foreground: yellowColor },
+
+        { token: 'keyword.throw', foreground: orangeColor },
+        { token: 'keyword.catch', foreground: orangeColor },
+        { token: 'keyword.try', foreground: orangeColor },
+
+        // returns
+        { token: 'keyword.returns', foreground: greenColor },
+        { token: 'keyword.return', foreground: greenColor },
+      ]
+      ,
+      colors: {
+
+      }
+      /*colors: {
+        // see https://code.visualstudio.com/api/references/theme-color for more settings
+        //'editor.background': darkColor,
+        /* 'editor.background': '#000000', // Set the background color
+         'editor.foreground': '#ffffff', // Set the foreground color
+         'editorSuggestWidget.background': lightColor,
+         'editorSuggestWidget.selectedBackground': secondaryColor,
+         'editorSuggestWidget.selectedForeground': textColor,
+         'editorSuggestWidget.highlightForeground': primaryColor,
+         'editorSuggestWidget.focusHighlightForeground': infoColor,
+         'editor.lineHighlightBorder': textbackground,
+         'editor.lineHighlightBackground': textbackground === darkColor ? lightColor : secondaryColor,
+         'editorGutter.background': lightColor,
+         //'editor.selectionHighlightBackground': secondaryColor,
+         'minimap.background': lightColor,
+         'menu.foreground': textColor,
+         'menu.background': textbackground,
+         'menu.selectionBackground': secondaryColor,
+         'menu.selectionForeground': textColor,
+         'menu.selectionBorder': secondaryColor,
+      },*/
+    })
+    monaco.editor.setTheme(themeName);
+
+
+
     monaco.languages.registerDocumentSymbolProvider(this.id, {
       // eslint-disable-next-line
       async provideDocumentSymbols(model, token): Promise<monaco.languages.DocumentSymbol[]> {
@@ -74,33 +227,39 @@ export default class Language implements monaco.languages.ILanguageExtensionPoin
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const result: monaco.languages.Hover = protocolToMonaco.asHover(response);
 
+        console.log("Hover result: ", result);
+
+        // add handler if hover result is null
+        let message = "";
+        if (result == null) {
+          message = "";
+        }
+        else {
+          message = result.contents[0].value;
+        }
 
 
+        // Create a decoration with the hover result
+        const decoration: monaco.editor.IModelDeltaDecoration = {
+          range: new monaco.Range(position.lineNumber, position.column, position.lineNumber, position.column),
+          options: {
+            hoverMessage: { value: message }
+          }
+        };
+
+        // Apply the decoration to the editor
+        model.deltaDecorations([], [decoration]);
 
 
         return result;
       },
+
     });
 
-    let mock_diagnostic: monaco.editor.IMarkerData = {
-      severity: monaco.MarkerSeverity.Error,
-      message: "This is a mock error",
-      startLineNumber: 1,
-      startColumn: 1,
-      endLineNumber: 5,
-      endColumn: 5,
-    }
-
-    //let diagnostic = monacoToProtocol.asDiagnostic(mock_diagnostic);
 
 
     monaco.editor.onDidCreateModel((model) => {
 
-      //let diagnostic = client.diagnostic;
-
-      //let markers = protocolToMonaco.asDiagnostics(diagnostic.diagnostics);
-
-      //monaco.editor.setModelMarkers(model, "solidity", markers);
 
       model.onDidChangeContent((event) => {
 
@@ -115,73 +274,11 @@ export default class Language implements monaco.languages.ILanguageExtensionPoin
 
       });
 
-
-
-      //monaco.editor.setModelMarkers(model, "solidity", [mock_diagnostic]);
     });
 
-    //monaco.editor.onDidChangeModelContent(async (event) => {
-
-    //monaco.editor.setModelMarkers(model, "solidity", [mock_diagnostic]);
 
 
 
-
-
-
-    /*monaco.languages.text(this.id, {
-      // eslint-disable-next-line
-      async provideCodeActions(model, range, context, token): Promise<monaco.languages.CodeActionList> {
-        void token;
-        const response = await (client.request(proto.DocumentDiagnosticRequest.type.method, {
-          textDocument: monacoToProtocol.asTextDocumentIdentifier(model),
-          range: monacoToProtocol.asRange(range),
-          context: {
-            diagnostics: [diagnostic],
-          },
-        } as proto.CodeActionParams) as Promise<proto.CodeAction>);
- 
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const result: monaco.languages.CodeActionList = protocolToMonaco.asDiagnostic(response);
- 
-        console.log("Code action result", result);
- 
-        return result;
-      },
-    });*/
-
-
-
-    monaco.languages.setMonarchTokensProvider('solidity', {
-      // Tokenizer
-      tokenizer: {
-        root: [
-          // natspec comments
-          [/@\*\*[\s\S]*?\*\//, 'comment.doc'],
-
-          // comments
-          [/\/\/.*/, 'comment'],
-
-          // operators
-          [/[\+\-\*\%\=\/\!\&\|\^\<\>\~]/, 'operator'],
-
-          // control keywords
-          [/\b(if|else|for|while|do|break|continue|return|throw|try|catch|finally|switch|case|default)\b/, 'keyword.control'],
-
-          // constants
-          [/\b(true|false|null|undefined)\b/, 'constant.language'],
-
-          // numbers
-          [/\b\d+\b/, 'number'],
-
-          // strings
-          [/".*?"/, 'string'],
-
-          // types
-          [/\b(bool|byte|address|int|uint|string|mapping|array)\b/, 'keyword.type'],
-        ],
-      },
-    });
 
 
 
